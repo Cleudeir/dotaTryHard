56195335c5de70b4cf365f4a5c46f3b55b6de925
cleudeir <cleudeirsilva@gmail.com>
2023-01-27 16:52:38 -0300

    esc bug full screen

--- pages/ranking.jsx
+++ pages/ranking.jsx
@@ -5,49 +5,44 @@ import Container from '../component/Container';
 import Head from 'next/head';
 import useRanking from './../hook/useRanking';
 
-const React = require('react');
+const React = require( 'react' );
 
 export async function getStaticProps() {
-  console.log('getStatic - Home: ');
-  const resp = await fetch(`${process.env.backUrl}/ranking?limit=1000`);
-  const data = await resp.json();
+  console.log( 'getStatic - Home: ' );
+  const resp = await fetch( `${ process.env.backUrl }/ranking?limit=1000` );
+  const {data, avgGlobal} = await resp.json();
   const regionsNames = ['WORLD', 'SOUTH AMERICA', 'NORTH AMERICA', 'EUROPE', 'CHINA'];
   const regionData = [];
-  for (let i = 0; i < regionsNames.length; i++) {
+  for ( let i = 0; i < regionsNames.length; i++ ) {
     const element = regionsNames[i];
-    if (element !== 'WORLD') {
-      const filter = data.filter((item) => item.profile.loccountrycode === element);
-      regionData.push(filter);
+    if ( element !== 'WORLD' ) {
+      const filter = data.filter( ( item ) => item.profile.loccountrycode === element );
+      regionData.push( filter );
     } else {
-      regionData.push(data);
+      regionData.push( data );
     }
   }
   return {
-    props: {regionData, regionsNames},
+    props: {regionData, regionsNames, avgGlobal},
     revalidate: 10 * 60,
   };
 }
 
-export default function Home({regionData, regionsNames}) {
-  const {filterRegion, isData, setData, filterName, order, isRegion} = useRanking({regionData});
-
+export default function Home( {regionData, regionsNames, avgGlobal} ) {
+  const {filterRegion, isData, setData, filterName, order, isRegion} = useRanking( {regionData} );
+  console.log('isData, avgGlobal: ', isData, avgGlobal);
   return (
-    <div className={styles.container}>
+    <div className={ styles.container }>
       <Head>
         <title>DotaTryHard</title>
         <meta name="description" content="DotaTryHard" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.png" />
       </Head>
-      <Header filterRegion={filterRegion} />
-      <Container isLoading={Boolean(isData)}>
-        <h2> {regionsNames[isRegion]} </h2>
-        <TableRanking
-          isData={isData}
-          filterName={filterName}
-          order={order}
-          setData={setData}
-        />
+      <Header filterRegion={ filterRegion } />
+      <Container isLoading={ Boolean( isData ) }>
+        <h2> { regionsNames[isRegion] } </h2>
+        <TableRanking isData={ isData } avgGlobal={avgGlobal} filterName={ filterName } order={ order } setData={ setData } />
       </Container>
     </div>
   );
